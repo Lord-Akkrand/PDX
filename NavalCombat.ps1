2a22fa56bdb53c18aaeee5fc7bef774e8bfd56ab
Antony Marshall <lordakkrand@gmail.com>
2020-07-06 11:29:14 +1000

    updated

--- NavalCombat.ps1
+++ NavalCombat.ps1
@@ -8,13 +8,10 @@ $global:FleetA = $null
 $global:FleetB = $null
 $global:Round = 0
 
-$PresetFleets = @()
-
-$PresetsXML = Get-Data (Join-Path -Path $PSScriptRoot -ChildPath "Presets.xml")
-
 function Fight($aFleet, $bFleet)
 {
     $global:FleetA = $aFleet
     $global:FleetB = $bFleet
-    Write-Host "Fight"
+    Write-Host("Fight {0} vs {1}" -F $aFleet.Name, $bFleet.Name)
+    
 }
--- TestFleets.ps1
+++ TestFleets.ps1
@@ -1,5 +1,6 @@
 param
 (
+    [String]$PresetsPath="Presets"
 )
 $ErrorActionPreference = 'Stop'
 $HomeLocation = $PWD
@@ -12,21 +13,29 @@ $NavalPath = $(Join-Path -Path $HomeLocation -ChildPath 'NavalCombat.ps1')
 . $UtilPath
 . $NavalPath
 
-function Test-Presets
+
+function Test-Presets()
 {
-    $PresetsXML = Get-Data (Join-Path -Path $PSScriptRoot -ChildPath "Presets.xml")
-    Write-Host $PresetsXML
-    foreach ($aXML in $PresetsXML.Fleets.ChildNodes)
+    $PresetsPath = Get-FleetPath $PresetsPath
+    $PresetFleets = @()
+
+    Get-ChildItem $PresetsPath -Filter *.xml | 
+    Foreach-Object {
+        Write-Host("Found <{0}>" -f $_.FullName)
+        $fleet = Import-Clixml -Path $_.FullName
+        $PresetFleets += $fleet
+    }
+
+    $progressId = 0
+    foreach($fleetA in $PresetFleets)
     {
-        Write-Host "FleetA"
-        foreach($bXML in $PresetsXML.Fleets.ChildNodes)
+        foreach($fleetB in $PresetFleets)
         {
-            Write-Host "FleetB"
-            $fleetA = Deep-Copy $aXML
-            $fleetB = Deep-Copy $bXML
+            $progressId++
             Fight $fleetA $fleetB
         }
     }
+    
 }
 
 Write-Host "Starting"
